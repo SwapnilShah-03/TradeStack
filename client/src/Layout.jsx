66a241e974ed30dd4b08525c2ce33a7dabd48789
GreninjaWarr235 <shah19349@gmail.com>
2023-10-27 10:03:32 +0530

    add loading page

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -3,8 +3,11 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import { Backdrop, CircularProgress } from "@mui/material";
+import { useNavigation } from "react-router-dom";
 
 export default function Layout() {
+  const navigation = useNavigation();
   return (
     <>
       <Header />
@@ -13,8 +16,22 @@ export default function Layout() {
         toastOptions={{ duration: 4000 }}
         containerClassName="font-Poppins"
       />
-      <Outlet />
-      <Footer />
+      {navigation.state === "loading" ? (
+        <Backdrop
+          sx={{
+            color: "#fff",
+          }}
+          open={true}
+        >
+          <div className="flex flex-col items-center justify-center gap-3 text-xs sm:text-base">
+            <CircularProgress color="inherit" />
+            Have patience, we are loading your data...
+          </div>
+        </Backdrop>
+      ) : (
+        <Outlet />
+      )}
+      {navigation.state !== "loading" && <Footer />}
     </>
   );
 }
